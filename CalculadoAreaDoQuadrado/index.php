952898009c1d105c798453d1ac71f91622c859b6
RamonPontes <rgdpontes@yahoo.com>
2025-02-13 17:07:34 -0300

    calculado da area do quadao php

---
+++ CalculadoAreaDoQuadrado/index.php
@@ -0,0 +1,13 @@
+<?php
+    $lado = $_POST["itlado"];
+    $area = $lado*$lado;
+?>
+<html>
+    <head>
+        <title>resultado</title>
+    </head>
+    <body>
+        <h1>Resultado</h1>
+        <p>A area do quadrado é <?php echo($area)?></p>
+    </body>
+</html>
